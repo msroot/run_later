c622af06b1adaae6698b6cd2471eed3442ea1a3e
Ioannis Kolovos <yannis.kolovos@gmail.com>
2015-04-26 15:27:07 +0200

    update

--- README.md
+++ README.md
@@ -24,7 +24,7 @@ TODO: Write usage instructions here
 
 ## Contributing
 
-1. Fork it ( https://github.com/[my-github-username]/run_later/fork )
+1. Fork it ( https://github.com/msroot/run_later/fork )
 2. Create your feature branch (`git checkout -b my-new-feature`)
 3. Commit your changes (`git commit -am 'Add some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
--- lib/run_later.rb
+++ lib/run_later.rb
@@ -9,43 +9,11 @@ module RunLater
   class CLI  < Thor     
     default_task :queue
     desc "queue", "Queue commands"
-    method_option :commands, :type => :string, :required => true
     def queue
-      FileUtils.mkdir_p(defaults[:root]) unless File.directory?(defaults[:root])
-        
-      open(defaults[:file], 'a+') do |f|
-        f.puts options[:commands]
-        say "Adding `#{options[:commands]}`", :green
-      end
-    end
-
-    desc "perform!", "Perform and save log"
-    def perform!
-      log = "#{@log}_#{Time.now.to_i}"
-      
-      open(log, 'w+') do |f|
-        open(defaults[:file], 'r').each_line do |command|
-          f.puts `#{command}`
-        end
-      end
+      say "Currently under development", :green
+      say "Check next version", :green
     end
 
 
-private
-def defaults
-  root = File.join(ENV['HOME'], '.run_later')
-  
-  {
-    root: File.join(ENV['HOME'], '.run_later'),
-    file: File.join(root, 'commands'),
-    log: File.join(root, 'log')
-  }
-end
-  
-  
-      
   end
 end
-
-
-# ruby -Ilib ./bin/run_later --commands="ls -la && pwd" 
