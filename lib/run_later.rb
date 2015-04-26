605282ebe52c43ed8949bc6b194ca5be9e89a145
Ioannis Kolovos <yannis.kolovos@gmail.com>
2015-04-26 13:35:32 +0200

    update

--- lib/run_later.rb
+++ lib/run_later.rb
@@ -11,51 +11,88 @@ module RunLater
     default_task :queue
     
     desc "queue", "Queue commands"
-    method_option :commands, :type => :string, :required => true, :alias=> :c
-    method_option :preload, :type => :array, :alias=> :p,:default => [ "~/.profile",  "~/.bash_profile"]
+    
+    method_option :commands, 
+    :type => :string, 
+    :alias=> :c,
+    :required => true, 
+    :banner => "'brew upgrade && brew install phantomjs'",
+    :desc => "Commands to execute"
+    
+    method_option :preload, 
+    :type => :array, 
+    :alias=> :p, 
+    :banner => "~/.bash_profile ~/.rvmrc", 
+    :default => [ "~/.profile",  "~/.bash_profile"], 
+    :desc => "Source files to be loaded before execution (separated by space)"
+    
+    method_option :sudo, 
+    :type => :boolean, 
+    :default => false,
+    :desc => "If commands require sudo"
+    
+    method_option :env, 
+    :type => :hash, 
+    :alias=> :e, 
+    :banner => "foo:bar joe:doe",
+    :desc => "System variables to be set before execution (key:value separated by space)"
+    
     def queue
       initialize_files!
       commands = get_commands
-      commands << options
+      commands << options.merge({
+        id: Time.now.to_i
+      })
+      
+      say "Adding `#{options[:commands]}`", :green
       
       open(defaults[:commands], 'w') do |f|
         f.write commands.to_yaml
-        say "Adding `#{options[:commands]}`", :green
       end
     end
 
     desc "perform", "Perform and save log"
     def perform
-
-      say "Script not found #{defaults[:commands]}", :red and return unless commands_exist?
+      initialize_files!
+      say "Empty", :green and return if get_commands.empty?
 
       log = defaults[:log]
 
       IO.popen("/bin/bash", "w") do |shell|
 
-      shell.puts("echo #{Time.now} >> #{log}")
+        shell.puts("echo #{Time.now} >> #{log}")
       
-      get_commands.map{ |command|
+        get_commands.map{ |command|
+          
+          say "-------------------------- Starting: #{command[:id]} --------------------------", :yellow
+          
+          
+          # load preloads
+          command[:preload].each {|source|
+            say "Loading: #{source}", :green
+            # shell.puts("source #{source}")
+            system("source #{source}")
+          }
+                
+          # load env
+          command[:env].each {|key, value|
+            env = "#{key}=#{value}"
+            say "Setting ENV: #{env}", :green
+            system("export #{env}")
+            # shell.puts("source #{source}")
+          }
         
-        # command[:preload].each {|source|
-        #           say "Loading: #{source}", :green
-        #           shell.puts("source #{source}")
-        #         }
         
-        
-        say "Running: #{command[:commands]}", :yellow
-        # shell.puts("sudo #{command[:commands]} >> #{log}")
-        # csh -c 'source ~/.profile && ls' -x    
-        exec "csh -c 'source #{command[:preload].join} && #{command[:commands]}' -x"
-        
-      }
+          say "Running: #{command[:commands]}", :yellow
+          # shell.puts("sudo #{command[:commands]} >> #{log}")
+          system("#{command[:commands]} >> #{log}")
 
-      say "Done! Log saved in #{log}", :green
+        }
+
+        say "Done! Log saved in #{log}", :green
+      end
     end
-    end
-    
-    
-    
+
 
     desc "clean", "clean"
     def clean
